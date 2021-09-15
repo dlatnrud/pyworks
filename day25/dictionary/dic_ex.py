a8f50a86eb11a581badb0469c733c8aea80df7cc
dlatnrud <dlatnrud2268@naver.com>
2021-09-15 13:10:12 +0900

    day26 업로드

--- day25/dictionary/dic_ex.py
+++ day25/dictionary/dic_ex.py
@@ -2,6 +2,7 @@
 
 dic = {1: 'A', 2: 'B', 3: 'C'}
 
+# 추가
 dic[4] = 'D'
 print(dic)
 
@@ -10,10 +11,10 @@ dic[4] = 'E'
 print(dic)
 
 # 삭제
-dic.pop(2)  # key를 선언
+dic.pop(2)      # key를 선언
 print(dic)
 
 # dic 요소 출력
 for key in dic:
     print(key)
-    print(dic[key]) # key, value 모두 출력
+    print(dic[key])     # key, value 모두 출력
--- day25/list/list2d.py
+++ day25/list/list2d.py
@@ -1,21 +1,20 @@
 # 이차원 리스트 - 리스트 내부에 또 리스트가 있음
-"""
+
 d2 = [
     [10, 20],   # 1행
     [30, 40]    # 2행
 ]
 # 인덱싱
-print(d2[0][0])   # d2 - 0행 0열
-print(d2[0][1])
-print(d2[1][0])
-print(d2[1][1])
+print(d2[0][0])     # d2 - 0행 0열(10)
+print(d2[0][1])     # d2 - 0행 1열(20)
+print(d2[1][0])     # d2 - 1행 0열(30)
+print(d2[1][1])     # d2 - 1행 1열(40)
 
 # 요소 값 전체 출력
 for x, y in d2:
     print(x,y)
+
 """
-
-
 # 일차원 리스트
 d1 = [10, 20, 30]
 print(d1)
@@ -24,11 +23,11 @@ for i in d1:
     print(i)
 
 # 추가
-d1.append(40)
+d1.append(40)   # [10, 20, 30, 40]
 print(d1)
 
 # 20을 삭제
-d1.remove(20)
+d1.remove(20)   # [10, 30, 40]
 print(d1)
 
 print(len(d1))   # 3
@@ -37,6 +36,7 @@ print(len(d1))   # 3
 sum_v = 0
 for i in d1:
     sum_v += i
-avg = sum_v / len(d1)
+avg = sum_v / len(d1)   # 80/3
 print("합계 :", sum_v)
 print("평균 : %.2f" % avg)
+"""
