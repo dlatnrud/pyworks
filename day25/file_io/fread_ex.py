40ed299748c7220df3065abafef43bd76ecf9d55
dlatnrud <dlatnrud2268@naver.com>
2021-09-17 13:18:52 +0900

    day28 업로그

--- day25/file_io/fread_ex.py
+++ day25/file_io/fread_ex.py
@@ -4,11 +4,20 @@
 
 try:
     with open("c:/pyfile/kbo2021.txt", 'r') as f:
-        # data = f.read()   # 전체 읽기
+        # data = f.read()       # 전체 읽기
+        # print(data)
+
+        line = f.readline()     # 한줄 읽기
+        print(line)
+
+        lines = f.readlines()   # 리스트로 반환
+        print(lines)
+        """
         while True:
             line = f.readline()    # 줄 단위로 읽어오기
             if not line:
                 break
             print(line)
+        """
 except FileNotFoundError:
     print("파일을 찾을 수 없습니다.")
